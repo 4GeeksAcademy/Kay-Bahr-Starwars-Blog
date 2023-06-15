9f18b7cbb1d35c4cd217fab4b95a1163875694e0
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-06-15 00:44:23 +0000

    detail fetch commit

--- src/js/views/planet-details.js
+++ src/js/views/planet-details.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/details.css";
@@ -11,33 +11,33 @@ export const PlanetDetails = () => {
 		actions.loadSomeData();
 	}, []);
 
-    const person = store.people.find(person => person.id === id);
+    const planet = store.planets.find(planet => planet.id === id);
 
-    if (!person) {
+    if (!planet) {
         return <div>Loading...</div>;
     }
 
     return (
         <div className="detailsBody">
-            <img src="..." className="card-img-top" alt={person.name} />
+            <img src="..." className="card-img-top" alt={planet.name} />
             <div className="detailsProfile">
-                <h2>{data.name}</h2>
+                <h2>{planet.name}</h2>
                 <p>Suspendisse potenti. Proin nisl orci, tincidunt sit amet efficitur non, mattis ut odio. Ut vitae placerat turpis, sit amet sodales est. Sed consectetur leo et eros dictum, eget mattis nisi pretium. Aenean condimentum arcu nisl, vitae ornare eros vehicula nec. Nunc lacinia luctus porta. Cras sit amet odio non lacus posuere feugiat sed id lorem. Suspendisse gravida accumsan libero a laoreet. Vestibulum egestas egestas purus, vitae dictum neque feugiat iaculis. Nullam mattis rhoncus ipsum, mattis blandit lectus interdum non. Fusce bibendum mauris in lorem finibus volutpat. Integer ornare accumsan aliquam. Maecenas viverra nulla in diam maximus pellentesque. Mauris eu ex ac magna pulvinar tristique sit amet in ex. Mauris iaculis aliquam mi, eu ultrices lacus vulputate eu.</p>
             </div>
             <div className="detailsBio">
                 <hr />
                 <p>
-                    {`Name: ${data.name}`}
+                    {`Name: ${planet.name}`}
                     <br />
-                    {`Climate: ${data.climate}`}
+                    {`Climate: ${planet.climate}`}
                     <br />
-                    {`Population: ${data.population}`}
+                    {`Population: ${planet.population}`}
                     <br />
-                    {`Orbital Period: ${data.orbital_period}`}
+                    {`Orbital Period: ${planet.orbital_period}`}
                     <br />
-                    {`Rotation Period: ${data.rotation_period}`}
+                    {`Rotation Period: ${planet.rotation_period}`}
                     <br />
-                    {`Diameter: ${data.diameter}`}
+                    {`Diameter: ${planet.diameter}`}
                 </p>
             </div>
         </div>
--- src/js/views/starship-details.js
+++ src/js/views/starship-details.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/details.css";
@@ -11,33 +11,33 @@ export const StarshipDetails = () => {
 		actions.loadSomeData();
 	}, []);
 
-    const person = store.people.find(person => person.id === id);
+    const starship = store.starships.find(starship => starship.id === id);
 
-    if (!person) {
+    if (!starship) {
         return <div>Loading...</div>;
     }
 
     return (
         <div className="detailsBody">
-            <img src="..." className="card-img-top" alt={person.name} />
+            <img src="..." className="card-img-top" alt={starship.name} />
             <div className="detailsProfile">
-                <h2>{store.starships.name}</h2>
+                <h2>{starship.name}</h2>
                 <p>Suspendisse potenti. Proin nisl orci, tincidunt sit amet efficitur non, mattis ut odio. Ut vitae placerat turpis, sit amet sodales est. Sed consectetur leo et eros dictum, eget mattis nisi pretium. Aenean condimentum arcu nisl, vitae ornare eros vehicula nec. Nunc lacinia luctus porta. Cras sit amet odio non lacus posuere feugiat sed id lorem. Suspendisse gravida accumsan libero a laoreet. Vestibulum egestas egestas purus, vitae dictum neque feugiat iaculis. Nullam mattis rhoncus ipsum, mattis blandit lectus interdum non. Fusce bibendum mauris in lorem finibus volutpat. Integer ornare accumsan aliquam. Maecenas viverra nulla in diam maximus pellentesque. Mauris eu ex ac magna pulvinar tristique sit amet in ex. Mauris iaculis aliquam mi, eu ultrices lacus vulputate eu.</p>
             </div>
             <div className="detailsBio">
                 <hr />
                 <p>
-                    {`Name: ${store.starships.name}`}
+                    {`Name: ${starship.name}`}
                     <br />
-                    {`Model: ${store.starships.model}`}
+                    {`Model: ${starship.model}`}
                     <br />
-                    {`Hyperdrive Rating: ${store.starships.hyperdrive_rating}`}
+                    {`Hyperdrive Rating: ${starship.hyperdrive_rating}`}
                     <br />
-                    {`Length: ${store.starships.length}`}
+                    {`Length: ${starship.length}`}
                     <br />
-                    {`Crew: ${store.starships.crew}`}
+                    {`Crew: ${starship.crew}`}
                     <br />
-                    {`Passengers: ${store.starships.passengers}`}
+                    {`Passengers: ${starship.passengers}`}
                 </p>
             </div>
         </div>
